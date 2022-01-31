ff5993bf03e9b85b6170f3ac2cc66c4d4b2a359b
YTC233 <2594418727@qq.com>
2022-01-31 11:57:40 +0800

    初版 部分还没弄好

---
+++ tools/geogebra/GeoGebra/HTML5/5.0/web/deferredjs/328BA5523FFF98FA22A33EAE45C91FF5/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback2('VBc(3391,1,a5l);_.te=function(){};Yhl(rk)(2);\n//# sourceURL=web-2.js\n')
---
+++ tools/geogebra/GeoGebra/HTML5/5.0/webSimple/deferredjs/DE359FFD48EB8DF7261CF8C1C3588B56/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('IDb(1937,1,oDh);_.Hd=function(){};l2g(Bi)(2);\n//# sourceURL=webSimple-2.js\n')
