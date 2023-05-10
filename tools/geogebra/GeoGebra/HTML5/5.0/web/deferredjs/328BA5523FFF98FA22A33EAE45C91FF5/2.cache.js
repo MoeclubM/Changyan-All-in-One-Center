1391b4bad60904d206de71054c84822a75ca143a
Moeclub <qytc233@gmail.com>
2023-05-10 20:37:13 +0800

    games

--- tools/geogebra/GeoGebra/HTML5/5.0/web/deferredjs/328BA5523FFF98FA22A33EAE45C91FF5/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.web.runAsyncCallback2('VBc(3391,1,a5l);_.te=function(){};Yhl(rk)(2);\n//# sourceURL=web-2.js\n')
--- tools/geogebra/GeoGebra/HTML5/5.0/webSimple/deferredjs/DE359FFD48EB8DF7261CF8C1C3588B56/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.webSimple.runAsyncCallback2('IDb(1937,1,oDh);_.Hd=function(){};l2g(Bi)(2);\n//# sourceURL=webSimple-2.js\n')
